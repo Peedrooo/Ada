b0dcff3cde10f7a6cbec537eebc9ae3fdbda1b36
PabloChristianno <pablochristianno@gmail.com>
2025-02-01 16:21:40 -0300

    feat: implementa classe csp

--- src/scripts/classCSP.py
+++ src/scripts/classCSP.py
@@ -1,16 +1,20 @@
 from constraint import constraint
+from variable import variable
 class classCSP:
-    def __init__(self):
+    def __init__(self, locals, days, times, cources):
         self.constraints
-        self.variable_list
+        self.variable_list = []
 
-    def init_variables():
+    def init_variables(self):
         """
         Gerar as turmas e adicionar varável na lista
         iniciar todas com mesmo domínio
         """
-        pass
+        # Hierarquia de ordenação: dia, horário, local
+        domain = [(local, day, horario) for day in self.days for horario in self.times for local in locals]
+        for cource in self.cources:
+            var = variable(cource, domain)
+            self.variable_list.append(var)
 
-    def init_constraint():
-        constraints = constraint()
-        pass
+    def init_constraint(self):
+        self.constraints = constraint()
